5c6ab79e86853c20f27a80ad3e7f848527255bbd
the1siddhant <36481542+the1siddhant@users.noreply.github.com>
2021-01-07 12:11:31 -0600

    Add files via upload

---
+++ randomProjectInJava/src/Finder.java
@@ -0,0 +1,38 @@
+
+import java.awt.Graphics;
+import java.awt.Color;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.text.Document;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author the1s
+ */
+public class Finder {
+    //private String URL;
+    public Finder() throws IOException, Exception{
+        TheFrame a = new TheFrame();
+//        try {
+//            //URL = "https://www.google.com/";
+//            URL url = new URL("https://www.google.com/");
+//            URLConnection urlconnection = url.openConnection();
+//            urlconnection.connect();
+//        } catch (MalformedURLException ex) {
+//            Logger.getLogger(Finder.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+    }
+    
+  
+}
---
+++ randomProjectInJava/src/firstClass.java
@@ -0,0 +1,20 @@
+
+import java.awt.Graphics;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author the1s
+ */
+public class firstClass {
+    public static void main(String[] args) throws Exception {
+        Finder a = new Finder();
+    }
+    
+    
+}
